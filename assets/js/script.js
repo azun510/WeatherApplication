f5d8e9db19f0ee06924a868c32ee3a0ee68daf93
Andrew Zuniga <azuniga183@gmail.com>
2021-05-22 17:35:19 -0700

    added temp hum ws

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,12 @@ var getCityWeather = function(city) {
     var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=de863e99a7b087a76baf123be83add0a"
 
 
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(response) {
+            weatherToday = response.main
+            document.querySelector("#weather-icon").setAttribute('src', "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png") 
+            document.querySelector("#feels-like").textContent = "Temperature: " + response.main.feels_like + "°F"
+            document.querySelector("#humidity").textContent = "Humidity: " + response.main.humidity + "%"
+            document.querySelector("#wind-speed").textContent = "Wind Speed: " + response.wind.speed
+            displayWeather(weatherToday, city)
+            

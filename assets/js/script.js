fefc14d04a1ac00bb0095ec7b105e1f0abac6b11
Andrew Zuniga <azuniga183@gmail.com>
2021-05-22 17:38:33 -0700

    added criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,27 @@ var getCityWeather = function(city) {
             document.querySelector("#wind-speed").textContent = "Wind Speed: " + response.wind.speed
             displayWeather(weatherToday, city)
             
+            var latitude = response.coord.lat
+            var longitude = response.coord.lon
+            
+            return fetch('http://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + "&units=imperial&appid=de863e99a7b087a76baf123be83add0a")
+                .then(function(response) {
+                    return response.json()
+                })
+                .then(function(response) {
+                    // uvi and index
+                    var uvEl = document.querySelector("#UV")
+                    var uvi = response.current.uvi
+                    uvEl.textContent = "UV Index: " + uvi
+
+                    if (uvi <2) {
+                        uvEl.classList.add("low");
+                      } else  if (uvi <5) {
+                        uvEl.classList.add("moderate")
+                      } else if (uvi <7) {
+                        uvEl.classList.add("high")
+                      } else if (uvi < 10) {
+                        uvEl.classList.add("veryhigh")
+                      } else {
+                        uvEl.classList.add("extreme")
+                      }
